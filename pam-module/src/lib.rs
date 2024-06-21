b38c1b93a0f1015629a7c8f77bea77ef7e9ac76a
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-06-21 16:48:54 -0700

    Cancel fingerprint authentication when unlocked by typing the password

--- pam-module/src/lib.rs
+++ pam-module/src/lib.rs
@@ -2,9 +2,11 @@ extern crate pam;
 extern crate rand;
 
 use std::ffi::CStr;
+use std::sync::mpsc::channel;
+use std::thread;
 
 use pam::constants::{PAM_ERROR_MSG, PamFlag, PamResultCode};
-use pam::constants::PamResultCode::{PAM_AUTH_ERR, PAM_SUCCESS};
+use pam::constants::PamResultCode::{PAM_ABORT, PAM_AUTH_ERR, PAM_SUCCESS};
 use pam::conv::Conv;
 use pam::module::{PamHandle, PamHooks};
 use pam::pam_try;
@@ -12,17 +14,92 @@ use pollster::block_on;
 use postcard::from_bytes;
 use zbus::blocking::Connection;
 
+use rust_fp::fingerprint_driver::{MatchedOutput, MatchOutput};
 use rust_fp_common::get_templates::get_templates;
 use rust_fp_common::rust_fp_proxy::RustFpProxyBlocking;
 use rust_fp_common::set_templates::set_templates;
-use rust_fp::fingerprint_driver::{MatchedOutput, MatchOutput};
 
-struct PamSober;
-pam::pam_hooks!(PamSober);
+use crate::wait_until_unlock::wait_until_unlock;
 
-impl PamHooks for PamSober {
+mod wait_until_unlock;
+
+struct RustFpPam;
+pam::pam_hooks!(RustFpPam);
+
+impl PamHooks for RustFpPam {
     // This function performs the task of authenticating the user.
     fn sm_authenticate(pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
+        enum Message {
+            Error(String),
+            Result(PamResultCode),
+        }
+
+        let (tx, rx) = channel();
+        // Exit on Ctrl+C
+        ctrlc::set_handler({
+            let tx = tx.clone();
+            move || {
+                // Useful for debugging
+                // Command::new("play").arg("https://www.myinstants.com/media/sounds/sudden-suspense-sound-effect.mp3").output().unwrap();
+                tx.send(Message::Result(PAM_ABORT)).unwrap();
+            }
+        }).unwrap();
+        // Exit if the screen was unlocked by typing the password
+        thread::spawn({
+            let tx = tx.clone();
+            move || {
+                wait_until_unlock();
+                // Useful for debugging
+                // Command::new("play").arg("https://www.myinstants.com/media/sounds/sudden-suspense-sound-effect.mp3").output().unwrap();
+                tx.send(Message::Result(PAM_ABORT)).unwrap();
+            }
+        });
+        // Actual fingerprint matching
+        thread::spawn({
+            let tx = tx.clone();
+            move || {
+                let authenticate = {
+                    let tx = tx.clone();
+                    move || -> PamResultCode {
+                        let mut templates = block_on(get_templates()).unwrap();
+                        if templates.len() > 0 {
+                            let connection = Connection::system().unwrap();
+                            let proxy = RustFpProxyBlocking::new(&connection).unwrap();
+                            let templates_vec = templates.iter().collect::<Vec<_>>();
+                            let max_attempts = 5;
+                            for attempt in 0..max_attempts {
+                                let output: MatchOutput = from_bytes(&proxy.match_templates(templates_vec.iter().map::<Vec<u8>, _>(|(_k, v)| v.to_vec()).collect()).unwrap()).unwrap();
+                                match output {
+                                    MatchOutput::Match(MatchedOutput { index, updated_template }) => {
+                                        let matched_label = templates_vec[index].0;
+                                        println!("Matched: {matched_label}.");
+                                        if let Some(updated_template) = updated_template {
+                                            println!("Template was updated. Saving updated template...");
+                                            templates.insert(matched_label.to_owned(), updated_template);
+                                            block_on(set_templates(&templates)).unwrap();
+                                            println!("Saved updated template");
+                                        }
+                                        return PAM_SUCCESS;
+                                    }
+                                    MatchOutput::NoMatch(error) => {
+                                        let remaining_attempts = max_attempts - attempt - 1;
+                                        tx.send(Message::Error(format!("No match. {remaining_attempts} attempts remaining."))).unwrap();
+                                        if let Some(error) = error {
+                                            tx.send(Message::Error(format!("Error matching: {error:?}"))).unwrap();
+                                        }
+                                    }
+                                }
+                            }
+                            PAM_AUTH_ERR
+                        } else {
+                            tx.send(Message::Error("No templates saved. Not matching.".into())).unwrap();
+                            PAM_AUTH_ERR
+                        }
+                    }
+                };
+                tx.send(Message::Result(authenticate())).unwrap();
+            }
+        });
         let conv = match pamh.get_item::<Conv>() {
             Ok(Some(conv)) => conv,
             Ok(None) => todo!(),
@@ -31,39 +108,16 @@ impl PamHooks for PamSober {
                 return err;
             }
         };
-        let mut templates = block_on(get_templates()).unwrap();
-        if templates.len() > 0 {
-            let connection = Connection::system().unwrap();
-            let proxy = RustFpProxyBlocking::new(&connection).unwrap();
-            let templates_vec = templates.iter().collect::<Vec<_>>();
-            let max_attempts = 5;
-            for attempt in 0..max_attempts {
-                let output: MatchOutput = from_bytes(&proxy.match_templates(templates_vec.iter().map::<Vec<u8>, _>(|(_k, v)| v.to_vec()).collect()).unwrap()).unwrap();
-                match output {
-                    MatchOutput::Match(MatchedOutput { index, updated_template }) => {
-                        let matched_label = templates_vec[index].0;
-                        println!("Matched: {matched_label}.");
-                        if let Some(updated_template) = updated_template {
-                            println!("Template was updated. Saving updated template...");
-                            templates.insert(matched_label.to_owned(), updated_template);
-                            block_on(set_templates(&templates)).unwrap();
-                            println!("Saved updated template");
-                        }
-                        return PAM_SUCCESS
-                    }
-                    MatchOutput::NoMatch(error) => {
-                        let remaining_attempts = max_attempts - attempt - 1;
-                        pam_try!(conv.send(PAM_ERROR_MSG, &format!("No match. {remaining_attempts} attempts remaining.")));
-                        if let Some(error) = error {
-                            pam_try!(conv.send(PAM_ERROR_MSG, &format!("Error matching: {error:?}")));
-                        }
-                    }
+        for message in rx {
+            match message {
+                Message::Error(message) => {
+                    pam_try!(conv.send(PAM_ERROR_MSG, &message));
+                }
+                Message::Result(result) => {
+                    return result;
                 }
             }
-            PAM_AUTH_ERR
-        } else {
-            pam_try!(conv.send(PAM_ERROR_MSG, "No templates saved. Not matching."));
-            PAM_AUTH_ERR
         }
+        PAM_AUTH_ERR
     }
 }
---
+++ pam-module/src/wait_until_unlock.rs
@@ -0,0 +1,22 @@
+use pollster::FutureExt;
+use zbus::{MatchRule, MessageStream};
+use zbus::export::futures_util::StreamExt;
+use zbus::message::Type;
+
+/// This function exits after unlock
+pub fn wait_until_unlock() {
+    let connection = zbus::connection::Connection::session().block_on().unwrap();
+    let match_rule = MatchRule::builder()
+        .member("ActiveChanged")
+        .unwrap()
+        .interface("org.freedesktop.ScreenSaver")
+        .unwrap()
+        .path("/ScreenSaver")
+        .unwrap()
+        .msg_type(Type::Signal)
+        .build();
+    let mut stream = Box::pin(MessageStream::for_match_rule(match_rule, &connection, None).block_on().unwrap()
+        .map(|message| message.unwrap().body().deserialize::<bool>().unwrap())
+        .filter(|&message| async move { !message }));
+    stream.next().block_on();
+}
