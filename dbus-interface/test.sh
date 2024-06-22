0a95c8a1aae060007ea8703bbc325ef7a1a15ef8
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-06-22 08:08:54 -0700

    Remove test scripts

--- dbus-interface/test.sh
+++
@@ -1,6 +0,0 @@
-#IP=192.168.124.164
-IP=192.168.1.185
-
-cargo build
-scp ./org.crosfp.CrosFp.conf root@$IP:/etc/dbus-1/system.d
-scp ../target/debug/dbus-interface root@$IP:/usr/local/bin/cros-dbus-interface
