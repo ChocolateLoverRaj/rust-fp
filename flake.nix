80321584922fc80ee3e31db4d5417efbff5b8ca8
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-07-05 11:16:47 -0700

    nixosModules in Nix Flake

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }: flake-utils.lib.eachDefaultSystem
+    (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
@@ -26,5 +26,100 @@
           ];
         };
       }
-    );
+    ) // {
+    nixosModules.default = { lib, pkgs, ... }: {
+      config =
+        let
+          rust-fp-dbus-interface-config = (pkgs.stdenv.mkDerivation rec {
+            name = "rust-fp-pam";
+            src = ./dbus-interface;
+            installPhase = ''
+              mkdir -p $out/share/dbus-1/system.d
+              cp $src/org.rust_fp.RustFp.conf $out/share/dbus-1/system.d
+              echo Cros FP Pam output at $out
+            '';
+          });
+          _cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "crosec-0.1.0" = "sha256-/G0/ClCZUdBv0a8fl/UUsXVCHD2V4Ts97oyQvfi23hE=";
+            };
+          };
+          rust-fp-dbus-interface = with pkgs; with builtins; rustPlatform.buildRustPackage
+            (
+              let
+                cargoToml = (fromTOML (readFile ./dbus-interface/Cargo.toml));
+              in
+              {
+                pname = cargoToml.package.name;
+                version = cargoToml.package.version;
+                cargoLock = _cargoLock;
+                src = ./.;
+                buildAndTestSubdir = "dbus-interface";
+                nativeBuildInputs = [
+                  rustPlatform.bindgenHook
+                  rustPlatform.cargoBuildHook
+                ];
+              }
+            );
+          rust-fp-cli = with pkgs; with builtins; rustPlatform.buildRustPackage (
+            let
+              cargoToml = (fromTOML (readFile ./cli/Cargo.toml));
+            in
+            {
+              pname = cargoToml.package.name;
+              version = cargoToml.package.version;
+              cargoLock = _cargoLock;
+              src = ./.;
+              buildAndTestSubdir = "cli";
+              nativeBuildInputs = [
+                rustPlatform.bindgenHook
+                rustPlatform.cargoBuildHook
+              ];
+            }
+          );
+          rust-fp-pam-module = with pkgs;  with builtins;rustPlatform.buildRustPackage (
+            let
+              cargoToml = (fromTOML (readFile ./pam-module/Cargo.toml));
+            in
+            {
+              pname = cargoToml.package.name;
+              version = cargoToml.package.version;
+              cargoLock = _cargoLock;
+              src = ./.;
+              buildAndTestSubdir = "pam-module";
+              nativeBuildInputs = [
+                rustPlatform.bindgenHook
+                rustPlatform.cargoBuildHook
+              ];
+              buildInputs = [
+                pam
+              ];
+            }
+          );
+        in
+        {
+          systemd.services.rust-fp-dbus-interface = {
+            enable = true;
+            description = "Gives normal user access to enrolling and matching fingerprints";
+            serviceConfig = {
+              Type = "exec";
+              ExecStart = "${rust-fp-dbus-interface}/bin/rust-fp-dbus-interface";
+            };
+            wantedBy = [ "multi-user.target" ];
+          };
+
+          # Example: https://github.com/NixOS/nixpkgs/issues/239770#issuecomment-1608589113
+          security.pam.services.kde-fingerprint.text = ''
+            auth    sufficient    ${rust-fp-pam-module}/lib/librust_fp_pam_module.so
+            account sufficient    ${rust-fp-pam-module}/lib/librust_fp_pam_module.so
+          '';
+
+          environment.systemPackages = [
+            rust-fp-dbus-interface-config
+            rust-fp-cli
+          ];
+        };
+    };
+  };
 }
